b5878005955575f6d4cbb8cfd554061778472ad0
Jaber-Saed <92320333+Jaber-Saed@users.noreply.github.com>
2023-08-30 19:38:35 +0300

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AllUsersComponent } from './all-users/all-users.component';
 import { UserDetailComponent } from './user-detail/user-detail.component';
+import { NewComponent } from './new/new.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AllUsersComponent,
-    UserDetailComponent
+    UserDetailComponent,
+    NewComponent
   ],
   imports: [
     BrowserModule
